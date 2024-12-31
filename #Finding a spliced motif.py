5ab9edf72c5e74ae81eecfa8fbae4e2c7203bd4e
Jonah Wilbur <158062037+Jonah-Wilbur@users.noreply.github.com>
2024-12-31 11:13:38 -0500

    Add files via upload

---
+++ #Finding a spliced motif.py
@@ -0,0 +1,25 @@
+#Finding a spliced motif
+def FASTA_to_str(FASTA):
+    string=''
+    for i in range(len(FASTA)):
+        if FASTA[i]!=' ':
+            string=string+FASTA[i]
+    return string
+def motif_search(string, motif):
+    output = []
+    j=0
+    for i in range(len(string)):
+        if string[i]==motif[j]:
+            output.append(i+1)
+            j=j+1
+        if j==len(motif):
+            break
+    if len(motif)==len(output):
+        print(True)
+    return output
+
+string = FASTA_to_str(input())
+motif = FASTA_to_str(input())
+list=motif_search(string,motif)
+for i in range(len(list)):
+    print(list[i], end=' ')
